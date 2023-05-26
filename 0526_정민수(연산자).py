989ea1392b9bf633094cd3165f9a0e44625ff085
ivernlover <134685630+ivernlover@users.noreply.github.com>
2023-05-26 16:58:24 +0900

    연산자_정민수

---
+++ 0526_정민수(연산자).py
@@ -0,0 +1,33 @@
+# 계산기 프로그램
+# 조건문
+# if 조건 :
+# elif 조건 :
+# else :
+'''
+a=int(input("a를 입력하세요:")) # input ()은 문자열을 입력받는다
+b=int(input("b를 입력하세요:"))
+op=input("연산자를 입력하세요:")
+if op=='+':
+    c=a+b
+    print(f"a+b={c}")
+elif op=='-':
+    c=a-b
+    print(f"a-b={c}")
+elif op=='*':
+    c=a*b
+    print(f"a*b={c}")
+elif op=='//':
+    c=a/b
+    print(f"a/b={c}")
+else:
+    print("연산자 오류")
+    
+'''
+# 음양수 판별 프로그램
+c=int(input("숫자를 입력해주세요:"))
+if c>0:
+    print("양수")
+elif c<0:
+    print("음수")
+else:
+    print("0")
